6055ae5742a2c7c9932caddca067c9460e62bfca
LTaskinen <lauri.taskinen2@metropolia.fi>
2025-01-16 10:29:13 +0200

    Tehtävä 2c done

---
+++ Moduuli 2/Tehtävä 2c.py
@@ -0,0 +1,6 @@
+Määre1 =float(input('Suorakulmion kanta?'))
+Määre2 =float(input('Suorakulmion korkeus?'))
+Piiri = Määre1 * 2 + Määre2 * 2
+Pinta = Määre1 * Määre2
+print(f'Suorakulmion piiri on: {Piiri}')
+print(f'Suorakulmion pinta-ala on: {Pinta}')
