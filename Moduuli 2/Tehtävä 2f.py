fac667c2a295ae434d1b02942b5d666e212abef3
LTaskinen <lauri.taskinen2@metropolia.fi>
2025-01-16 11:35:07 +0200

    Tehtävä 2e done

---
+++ Moduuli 2/Tehtävä 2f.py
@@ -0,0 +1,5 @@
+import random
+lock19 = random.randint(0,9), random.randint(0,9), random.randint(0,9)
+lock16 = random.randint(1,6), random.randint(1,6), random.randint(1,6), random.randint(1,6)
+print(lock19)
+print(lock16)
