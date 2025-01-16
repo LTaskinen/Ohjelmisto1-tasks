7a2f738ca77bae39e3e8e1da16f7e5add8e15fd4
LTaskinen <lauri.taskinen2@metropolia.fi>
2025-01-16 10:43:36 +0200

    Tehtävä 2d done

---
+++ Moduuli 2/Tehtävä 2d.py
@@ -0,0 +1,10 @@
+luku1 = float(input('Anna ensimmäinen luku:'))
+luku2 = float(input('Anna toinen luku:'))
+luku3 = float(input('Anna kolmas luku:'))
+Summa = luku1 + luku2 + luku3
+Keskiarvo1 = luku1 + luku2 + luku3
+Keskiarvo2 = Keskiarvo1/3
+Tulo = luku1 * luku2 * luku3
+print(f'Lukujen summa on: {Summa}')
+print(f'Lukujen tulo on: {Tulo}')
+print(f'Lukujen keskiarvo on: {Keskiarvo2}')
