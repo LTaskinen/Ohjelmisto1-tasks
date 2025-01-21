78b12dbe606e27d6939261fd5b416e32041858cd
LTaskinen <lauri.taskinen2@metropolia.fi>
2025-01-21 13:05:08 +0200

    Tehtävä 4.1 done

---
+++ Moduuli 4/Tehtävä 4.2.py
@@ -0,0 +1,6 @@
+inches = float(input('Anna tuumat:'))
+while inches > 0:
+    print(f"{inches} tuumaa = {inches*2.54}cm")
+    inches = float(input('Anna tuumat:'))
+else:
+    print('Toiminta lopetetaan.')
