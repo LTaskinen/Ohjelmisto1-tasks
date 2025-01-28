25effa58c34ce5a04b82392e8a8127f67004c1cd
LTaskinen <lauri.taskinen2@metropolia.fi>
2025-01-28 14:30:32 +0200

    Tehtävä 5.3 vaihe1

--- Moduuli 5/Tehtävä 5.3.py
+++ Moduuli 5/Tehtävä 5.3.py
@@ -1,3 +1,9 @@
-import math
-luku1 = float(input('Syötä kokonaisluku:'))
-for i in range(math.sqrt(luku1),1,-1):
+luku1 = int(input('Syötä kokonaisluku:'))
+if luku1 <= 1:
+    print('Luku ei ole alkuluku')
+else:
+    for luku in range(2,int(luku1 ** 0.5),+1):
+        if luku1 % luku == 0:
+            print('Luku ei ole alkuluku')
+        else:
+            print('Luku on alkuluku')
